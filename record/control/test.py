b7b1fb97e86bfed4d8e76bf8a075efdf7025be2f
cockroach <yyx198907@gmail.com>
2019-07-21 00:25:32 +0800

    条件语句学习

--- record/control/test.py
+++ record/control/test.py
@@ -1,16 +1,17 @@
-# 链式赋值
-k = t = ['a', 'b']
-z = 'a'
-v = 'b'
+rule80 = 80
+rule120 = 120
+rule160 = 160
 
-if condition:
-    pass
+
+pace = input('当前数据：')
+
+if rule80 <= pace and rule120 >= pace:
+    print('平安驾驶,当前速度：{}'.format(pace))
+
+elif rule80 > pace or rule120 <= pace and rule160 > pace:
+    print('危险驾驶,当前速度：{}'.format(pace))
+
+elif rule160 <= pace:
+    print('严重超速,当前速度：{}'.format(pace))
 else:
-    pass
-
-
-
-
-
-
-
+    print('数据错误')
