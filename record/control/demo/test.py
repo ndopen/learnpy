f8e4420504492d755802abe12fe08eb2cc579f0b
cockroach <yyx198907@gmail.com>
2019-07-31 00:05:28 +0800

    循环相关基础知识

--- record/control/demo/test.py
+++ record/control/demo/test.py
@@ -33,10 +33,48 @@
 # print(sorted(s, key=str.lower))
 # print(list(reversed(s)))
 
-from math import sqrt
+# 找出小于100的最大平方值
+# from math import sqrt
+# for n in range(99, 0, -1):
+#     root = sqrt(n)
+#     if root == int(root):
+#         print(n)
+#         break
 
-for n in range(99, 0, -1):
-    root = sqrt(n)
-    if root == int(root):
-        print(n)
-        break
+
+#　while为true,执行循环代码，用户无任何输入，代码继续循环，word为空值
+# word = 'hello'
+# while word:
+#     word = input('please enter a word:')
+#     print('the word was', word)
+
+# while 始终为true,执行循环内代码,if not word判断用户输入情况，如not word为true时执行break结束循环
+# while True:
+#     word = input('please enter a word:')
+#     if not word: 
+#         break
+#     print(word)
+
+# while循环 else执行控制
+# from math import sqrt
+# for n in range(99, 81, -1):
+#     root = sqrt(n)
+#     if root == int(root):
+#         print(n)
+#         break
+# else:
+#     print('while else')
+
+girls = ['alice', 'bernice', 'clarice']
+boys = ['chris', 'arnold', 'bob']
+letterGirls = {}
+
+for girl in girls:
+    letterGirls.setdefault(girl[0], []).append(girl)
+print([b+'+'+g for b in boys for g in letterGirls[b[0]]])
+print(letterGirls)
+
+for target_list in expression_list:
+    pass
+else:
+    pass
