d89d0edb875002d381652eebecc4c5c7644c52bc
cockroach <yyx198907@gmail.com>
2019-08-07 23:39:42 +0800

    funtion01

--- PythonNotes/function base.md
+++ PythonNotes/function base.md
@@ -1,2 +1,25 @@
-# function basics
+# 目录
+* [callable函数](http://#callable)
+* [createFuntion](http://#CreateFuntion)
+
+# callable
 > 函数是结构化编程的核心知识，函数在执行特定化任务的时候会返回值，但并非所有定义的函数将一定会返回值,要判定函数是否可用使用内置函数callable
+```python
+>>> import math
+>>> x = 1
+>>> y = math.sqrt
+>>> callable(x)
+False
+>>> callable(y)
+True
+```
+
+# CreateFuntion
+> 使用def关键字定义函数
+```python
+# CreateFuntion
+def hello(name):
+    return 'hello' + name
+hello('cock')
+```
+
--- PythonNotes/test.py
+++ PythonNotes/test.py
@@ -1,17 +1,21 @@
-rule80 = 80
-rule120 = 120
-rule160 = 160
+# 斐波那契数(每个列表数据都是前两个的和)
+# fids = [0, 1]
+# num = int(input('How many Fibonacci numbers do you want? '))
 
+# for i in range(num-2):
+#     fids.append(fids[-2] + fids[-1])
+# print(fids)
 
-pace = input('当前数据：')
+# def fibs(num):
+#      result = [0, 1]
+#      for i in range(num-2):
+#          result.append(result[-2] + result[-1])
+#      return result
+# fibs(20)
 
-if rule80 <= pace and rule120 >= pace:
-    print('平安驾驶,当前速度：{}'.format(pace))
+# print(callable(fibs))
 
-elif rule80 > pace or rule120 <= pace and rule160 > pace:
-    print('危险驾驶,当前速度：{}'.format(pace))
-
-elif rule160 <= pace:
-    print('严重超速,当前速度：{}'.format(pace))
-else:
-    print('数据错误')
+# CreateFuntion
+def hello(name):
+    return 'hello' + name
+hello('cock')
