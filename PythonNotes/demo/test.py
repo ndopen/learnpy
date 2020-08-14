b8c4a915ec5bf67918ad1782888f2acf927d2e8d
cockroach <yyx198907@gmail.com>
2020-08-14 21:27:04 +0800

    del project

--- PythonNotes/demo/test.py
+++ PythonNotes/demo/test.py
@@ -65,16 +65,18 @@
 # else:
 #     print('while else')
 
-girls = ['alice', 'bernice', 'clarice']
-boys = ['chris', 'arnold', 'bob']
-letterGirls = {}
+# girls = ['alice', 'bernice', 'clarice']
+# boys = ['chris', 'arnold', 'bob']
+# letterGirls = {}
 
-for girl in girls:
-    letterGirls.setdefault(girl[0], []).append(girl)
-print([b+'+'+g for b in boys for g in letterGirls[b[0]]])
-print(letterGirls)
+# for girl in girls:
+#     letterGirls.setdefault(girl[0], []).append(girl)
+# print([b+'+'+g for b in boys for g in letterGirls[b[0]]])
+# print(letterGirls)
 
-for target_list in expression_list:
-    pass
-else:
-    pass
+# for target_list in expression_list:
+#     pass
+# else:
+#     pass
+
+print('qer')
